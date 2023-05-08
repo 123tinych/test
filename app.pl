d3446348b54cddd487f2686a263f7f0b9ad37b12
tin <123tin@gmail.com>
2023-05-08 16:24:58 +0300

    2 loop

--- app.pl
+++ app.pl
@@ -2,8 +2,8 @@
  use POSIX;
  use feature 'say';
 
-#$unical = rand(10);
-#for (;;) {
-say strftime ("%Y-%m-%d %H:%M:%S", localtime time) . " " . rand(10);
-#sleep(5);
-#}
+$unical = rand(10);
+while (1) {
+say strftime ("%Y-%m-%d %H:%M:%S", localtime time) . " " . $unical;
+sleep(5);
+}
